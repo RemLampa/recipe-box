739ce63c2739413269316d0140109e8514680e25
ibleedfilm <rem@lansangan.com>
2017-02-18 17:41:28 +0800

    Add RecipeList showModal() method

--- src/components/RecipeList/index.js
+++ src/components/RecipeList/index.js
@@ -15,13 +15,31 @@ export default class RecipeList extends Component {
             'water'
           ]
         }
-      ]
+      ],
+      recipeModal: {
+        isHidden: true,
+        selectedRecipe: null
+      }
     };
+
+    this.addRecipe = this.addRecipe.bind(this);
+    this.showModal = this.showModal.bind(this);
   }
 
   addRecipe(recipe) {
     const recipes = [ ...this.state.recipes, recipe ];
     this.setState({ recipes });
+  }
+
+  showModal(recipeId) {
+    const recipeModal = {
+      isHidden: false,
+      selectedRecipe: recipeId || recipeId === 0 ?
+        this.state.recipes[recipeId] :
+        null
+    };
+
+    this.setState({ recipeModal });
   }
 
   render() {
@@ -30,6 +48,7 @@ export default class RecipeList extends Component {
         <ul>
           <li>Test</li>
         </ul>
+        <button>Create Recipe</button>
       </div>
     );
   }
--- test/components/RecipeList/index.spec.js
+++ test/components/RecipeList/index.spec.js
@@ -15,7 +15,11 @@ describe('<RecipeList />', () => {
             'water'
           ]
         }
-      ]
+      ],
+      recipeModal: {
+        isHidden: true,
+        selectedRecipe: null
+      }
     };
 
     wrapper = shallow(<RecipeList />);
@@ -28,9 +32,10 @@ describe('<RecipeList />', () => {
 
   it('should have proper initial state', () => {
     expect(wrapper).to.have.state('recipes').deep.equal(initialState.recipes);
+    expect(wrapper).to.have.state('recipeModal').deep.equal(initialState.recipeModal);
   });
 
-  it('should have method addRecipe() that adds new recipe to this.state.recipes', () => {
+  it('should have method addRecipe() that adds new recipe to recipes state', () => {
     const recipe = {
       name: 'Test recipe',
       description: 'This is a test recipe',
@@ -47,11 +52,69 @@ describe('<RecipeList />', () => {
     expect(wrapper).to.have.state('recipes').deep.equal(initialState.recipes);
   });
 
-  it('should have a button that calls method addRecipe()', () => {
+  it('should have a method showModal() that properly sets recipeModal state', () => {
+    const newState = {
+      recipes: [
+        ...initialState.recipes,
+        {
+          name: 'Test recipe',
+          description: 'This is a test recipe',
+          ingredients: [
+            'pizza',
+            'spaghetti',
+            'drinks'
+          ]
+        },
+        {
+          name: 'Test recipe2',
+          description: 'This is a test recipe2',
+          ingredients: [
+            'pizzas',
+            'spaghettis',
+            'drinks'
+          ]
+        }
+      ]
+    };
 
+    wrapper.setState({ recipes: newState.recipes });
+
+    instance.showModal();
+
+    expect(wrapper).to.have.state('recipeModal').deep.equal({
+      isHidden: false,
+      selectedRecipe: null
+    });
+
+    [0, 1, 2].map(id => {
+      wrapper.setState({ recipeModal: initialState.recipeModal });
+
+      instance.showModal(id);
+
+      expect(wrapper).to.have.state('recipeModal').deep.equal({
+        isHidden: false,
+        selectedRecipe: newState.recipes[id]
+      });
+    });
   });
 
   it('should contain a ul', () => {
     expect(wrapper).to.have.exactly(1).descendants('ul');
+  });
+
+  it('should contain a button', () => {
+    expect(wrapper).to.have.exactly(1).descendants('button');
+  });
+
+  context('Add New Recipe Button', () => {
+    let buttonWrapper;
+
+    beforeEach(() => {
+      buttonWrapper = wrapper.find('button');
+    });
+
+    it('should have text "Create Recipe"', () => {
+      expect(buttonWrapper).to.have.text('Create Recipe');
+    });
   });
 });

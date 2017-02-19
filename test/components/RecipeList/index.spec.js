24957f4dad93071c19c6d4bd54fe218b93421c2e
ibleedfilm <rem@lansangan.com>
2017-02-19 13:35:42 +0800

    Refactor RecipeList tests

--- test/components/RecipeList/index.spec.js
+++ test/components/RecipeList/index.spec.js
@@ -51,16 +51,6 @@ describe('<RecipeList />', () => {
 
     wrapper = shallow(<RecipeList />);
     instance = wrapper.instance();
-
-    showModal = () => {
-      const recipeModal = {
-        isHidden: false,
-        selectedRecipe: newState.recipes[1],
-        mode: 'read'
-      };
-
-      instance.setState({ recipeModal });
-    }
   });
 
   it('should be a div', () => {
@@ -72,115 +62,130 @@ describe('<RecipeList />', () => {
     expect(wrapper).to.have.state('recipeModal').deep.equal(initialState.recipeModal);
   });
 
-  it('should have method createRecipe() that adds new recipe to recipes state', () => {
-    const recipe = {
-      name: 'Test recipe',
-      description: 'This is a test recipe',
-      ingredients: [
-        'pizza',
-        'spaghetti',
-        'drinks'
-      ]
-    };
+  describe('CRUD Methods', () => {
+    let hideModalSpy;
 
-    showModal();
-    initialState.recipes.push(recipe);
-    instance.createRecipe(recipe);
-
-    expect(wrapper).to.have.state('recipes').deep.equal(initialState.recipes);
-    expect(wrapper).to.have.state('recipeModal').deep.equal(initialState.recipeModal);
-  });
-
-  it('should have a method updateRecipe() that updates recipes state', () => {
-    const newRecipe = {
-      name: 'Updated recipe',
-      description: 'This is an updated recipe',
-      ingredients: [
-        'updated pizza',
-        'updated spaghetti',
-        'updated drinks'
-      ]
-    };
-
-    showModal();
-
-    instance.setState({ recipes: [...newState.recipes] });
-
-    newState.recipes[1] = newRecipe;
-
-    instance.updateRecipe(1, newRecipe);
-
-    expect(wrapper).to.have.state('recipes').deep.equal(newState.recipes);
-    expect(wrapper).to.have.state('recipeModal').deep.equal(initialState.recipeModal);
-  });
-
-  it('should have a method deleteRecipe() that updates recipes state', () => {
-    showModal();
-
-    instance.setState({ recipes: [...newState.recipes] });
-
-    newState.recipes.splice(1,1);
-
-    instance.deleteRecipe(1);
-
-    expect(wrapper).to.have.state('recipes').deep.equal(newState.recipes);
-    expect(wrapper).to.have.state('recipeModal').deep.equal(initialState.recipeModal);
-  });
-
-  it('should have method showModal() that sets recipeModal state (incl. CRUD)', () => {
-    wrapper.setState({ recipes: [...newState.recipes] });
-
-    instance.showModal(null, 'create');
-
-    expect(wrapper).to.have.state('recipeModal').deep.equal({
-      isHidden: false,
-      selectedRecipe: null,
-      mode: 'create'
+    beforeEach(() => {
+      hideModalSpy = spy(instance, 'hideModal');
     });
 
-    [0, 1, 2].map(id => {
-      wrapper.setState({ recipeModal: initialState.recipeModal });
+    afterEach(() => {
+      hideModalSpy.restore();
+    });
 
-      instance.showModal(id, 'read');
+    it('should have method createRecipe() that updates recipes state', () => {
+      const recipe = {
+        name: 'Test recipe',
+        description: 'This is a test recipe',
+        ingredients: [
+          'pizza',
+          'spaghetti',
+          'drinks'
+        ]
+      };
+
+      initialState.recipes.push(recipe);
+      instance.createRecipe(recipe);
+
+      expect(wrapper).to.have.state('recipes').deep.equal(initialState.recipes);
+      expect(hideModalSpy).to.have.been.calledOnce;
+    });
+
+    it('should have method updateRecipe() that updates recipes state', () => {
+      const newRecipe = {
+        name: 'Updated recipe',
+        description: 'This is an updated recipe',
+        ingredients: [
+          'updated pizza',
+          'updated spaghetti',
+          'updated drinks'
+        ]
+      };
+
+      instance.setState({ recipes: [...newState.recipes] });
+
+      newState.recipes[1] = newRecipe;
+
+      instance.updateRecipe(1, newRecipe);
+
+      expect(wrapper).to.have.state('recipes').deep.equal(newState.recipes);
+      expect(hideModalSpy).to.have.been.calledOnce;
+    });
+
+    it('should have method deleteRecipe() that updates recipes state', () => {
+      instance.setState({ recipes: [...newState.recipes] });
+
+      newState.recipes.splice(1,1);
+
+      instance.deleteRecipe(1);
+
+      expect(wrapper).to.have.state('recipes').deep.equal(newState.recipes);
+      expect(hideModalSpy).to.have.been.calledOnce;
+    });
+  });
+
+  describe('Modal Methods', () => {
+    it('should have method showModal() that sets recipeModal state (incl. CRUD)', () => {
+      wrapper.setState({ recipes: [...newState.recipes] });
+
+      instance.showModal(null, 'create');
 
       expect(wrapper).to.have.state('recipeModal').deep.equal({
         isHidden: false,
-        selectedRecipe: newState.recipes[id],
+        selectedRecipe: null,
+        mode: 'create'
+      });
+
+      [0, 1, 2].map(id => {
+        wrapper.setState({ recipeModal: initialState.recipeModal });
+
+        instance.showModal(id, 'read');
+
+        expect(wrapper).to.have.state('recipeModal').deep.equal({
+          isHidden: false,
+          selectedRecipe: newState.recipes[id],
+          mode: 'read'
+        });
+      });
+
+      [0, 1, 2].map(id => {
+        wrapper.setState({ recipeModal: initialState.recipeModal });
+
+        instance.showModal(id, 'update');
+
+        expect(wrapper).to.have.state('recipeModal').deep.equal({
+          isHidden: false,
+          selectedRecipe: newState.recipes[id],
+          mode: 'update'
+        });
+      });
+
+      [0, 1, 2].map(id => {
+        wrapper.setState({ recipeModal: initialState.recipeModal });
+
+        instance.showModal(id, 'delete');
+
+        expect(wrapper).to.have.state('recipeModal').deep.equal({
+          isHidden: false,
+          selectedRecipe: newState.recipes[id],
+          mode: 'delete'
+        });
+      });
+    });
+
+    it('should have method hideModal() the resets recipeModal state', () => {
+      const recipeModal = {
+        isHidden: false,
+        selectedRecipe: newState.recipes[1],
         mode: 'read'
-      });
+      };
+
+      instance.setState({ recipeModal });
+
+      instance.hideModal();
+
+      expect(wrapper).to.have.state('recipeModal').deep.equal(initialState.recipeModal);
     });
-
-    [0, 1, 2].map(id => {
-      wrapper.setState({ recipeModal: initialState.recipeModal });
-
-      instance.showModal(id, 'update');
-
-      expect(wrapper).to.have.state('recipeModal').deep.equal({
-        isHidden: false,
-        selectedRecipe: newState.recipes[id],
-        mode: 'update'
-      });
-    });
-
-    [0, 1, 2].map(id => {
-      wrapper.setState({ recipeModal: initialState.recipeModal });
-
-      instance.showModal(id, 'delete');
-
-      expect(wrapper).to.have.state('recipeModal').deep.equal({
-        isHidden: false,
-        selectedRecipe: newState.recipes[id],
-        mode: 'delete'
-      });
-    });
-  });
-
-  it('should have method hideModal() the resets recipeModal state', () => {
-    showModal();
-
-    instance.hideModal();
-
-    expect(wrapper).to.have.state('recipeModal').deep.equal(initialState.recipeModal);
   });
 
   it('should contain a ul', () => {
@@ -205,7 +210,7 @@ describe('<RecipeList />', () => {
     expect(wrapper).to.have.exactly(1).descendants('button');
   });
 
-  context('Add New Recipe Button', () => {
+  describe('Add New Recipe Button', () => {
     let buttonWrapper;
 
     beforeEach(() => {

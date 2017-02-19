66186930369ec1d15bc52eb77151886a3e2405f4
ibleedfilm <rem@lansangan.com>
2017-02-19 12:26:13 +0800

    Add React.PropTypes to RecipeModal

--- src/components/RecipeModal/index.js
+++ src/components/RecipeModal/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 import { Modal } from 'react-bootstrap';
 
-const RecipeModal = ({isHidden, recipe}) => (
+const RecipeModal = (props) => (
   <Modal
-    show={!isHidden}
+    show={!props.isHidden}
     bsSize='lg'
-    backdrop={true}
+    backdrop='static'
   >
     <Modal.Body>
       Test
     </Modal.Body>
   </Modal>
 );
+
+RecipeModal.propTypes = {
+  isHidden: PropTypes.bool.isRequired,
+  recipe: PropTypes.object,
+  mode: PropTypes.string.isRequired,
+  onCreate: PropTypes.func.isRequired,
+  onUpdate: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
+  onHide: PropTypes.func.isRequired
+}
 
 export default RecipeModal;
